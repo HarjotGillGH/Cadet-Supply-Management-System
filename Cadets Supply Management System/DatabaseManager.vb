cc4d3b09cc2a9fb91f1d7462cd99342be70a4ab4
Harjot Gill <806430@gmail.com>
2024-02-21 18:36:08 -0800

    added diary file and database manager file

---
+++ Cadets Supply Management System/DatabaseManager.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.Data.Sqlite
+Public Class DatabaseManager
+
+    Private connectionString As String
+
+    Public Sub New(ByVal connectionStr As String)
+        Me.connectionString = connectionStr
+    End Sub
+
+    ' intialize connection
+    ' insert data
+    Public Function runQuery(ByVal query As String)
+        Dim dataTable = New DataTable()
+        Using connection As New SqliteConnection(connectionString)
+            connection.Open()
+            Using command As New SqliteCommand(query, connection)
+
+                Using reader As SqliteDataReader = command.ExecuteReader()
+                    dataTable.Load(reader)
+
+                End Using
+            End Using
+        End Using
+        Return DataTable
+    End Function
+
+
+End Class
